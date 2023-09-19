c65905cf618a89640c5781e5d63a6b8ebddf3c7d
Josué Durand pebe <josue.durand8@gmail.com>
2023-09-19 10:19:21 -0500

    nagivation

--- app/src/main/java/com/durand/composecomponentandroid/HomeScreen.kt
+++ app/src/main/java/com/durand/composecomponentandroid/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -34,10 +35,10 @@ fun HomeScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        TextField(
+        OutlinedTextField(
             value = text,
             onValueChange = { text = it },
-            modifier = Modifier.fillMaxWidth()
+            label = { Text(text = "Name")}
         )
         Button(onClick = { onButtonClick(text) }, modifier = Modifier.fillMaxWidth()) {
             Text(text = "Enviar")

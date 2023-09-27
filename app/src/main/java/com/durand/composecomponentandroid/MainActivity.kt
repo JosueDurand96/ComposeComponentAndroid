71aa970c1ccbe70dfb6455b414cb687d761c7575
Josue Durand Pebe <josue.durand_encora@scotiabank.com.pe>
2023-09-26 20:41:52 -0500

    clean

--- app/src/main/java/com/durand/composecomponentandroid/MainActivity.kt
+++ app/src/main/java/com/durand/composecomponentandroid/MainActivity.kt
@@ -6,15 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.durand.composecomponentandroid.ui.theme.ComposeComponentAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,26 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "home") {
-                        composable(route = "home") {
-                            HomeScreen {
-                                navController.navigate("detail/$it ")
-                            }
-                        }
-                        composable(
-                            route = "detail/{body}",
-                            arguments = listOf(
-                                navArgument("body") {
-                                    type = NavType.StringType
-                                }
-                            )
-                        ) {
-                            val body = it.arguments?.getString("body") ?: ""
-
-                            DetailScreen(body)
-                        }
-                    }
+                    HomeScreen()
                 }
             }
         }
